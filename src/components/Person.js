e5c0c51832878c467e8c3b0d32267744bc5d3542
Christopher <christopher.loubassou@hotmail.fr>
2019-07-16 13:28:24 +0200

    Update from the Crud Done

--- src/components/Person.js
+++ src/components/Person.js
@@ -14,7 +14,7 @@ class Person extends Component {
             firstName: this.props.person.firstName,
             lastName: this.props.person.lastName
         });
-    }
+    };
 
     // Update value from a person
     editPerson = () => {
@@ -37,19 +37,53 @@ class Person extends Component {
             lastName: e.target.value
         });
     };
+    
+    handleUpdate = (e) => {
+        e.preventDefault();
+        this.setState({
+            isEditing: false
+        })
+    };
+
 
     render() {
 
         // Let's destructuring
-        const { firstName, lastName } = this.state;
+        const { firstName, lastName, isEditing } = this.state;
         const { removePerson } = this.props;
 
         return (
             <div>
-                <li>FirstName: {firstName} </li>
-                <li>LastName: {lastName}</li>
-                <button onClick={(e) => removePerson(this.props.person)}>Remove</button>
-                <button>Edit</button>
+                {
+                    isEditing ? (
+                        // Form to Update person
+                        <form onSubmit={this.handleUpdate}>
+                            <input
+                                type="text"
+                                placeholder="first name"
+                                value={firstName}
+                                onChange={this.handleUpdateFirstName}
+                            />
+                            <br /><br />
+                            <input
+                                type="text"
+                                placeholder="last name"
+                                value={lastName}
+                                onChange={this.handleUpdateLastName}
+                            />
+                            <br /><br />
+                            <button>Update</button>
+                        </form>
+                    ) : (
+                        // View of the person
+                        <div>
+                            <li>FirstName: {firstName} </li>
+                            <li>LastName: {lastName}</li>
+                            <button onClick={(e) => removePerson(this.props.person)}>Remove</button>
+                            <button onClick={this.editPerson}>Edit</button>
+                        </div>
+                    )
+                }
             </div>
         );
     }
