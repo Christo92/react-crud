c893054667f4787be3d4c8fa8254c3540e874707
Christopher <christopher.loubassou@hotmail.fr>
2019-11-02 18:45:50 +0100

    Form component change to fonctional component

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useRef } from 'react';
 // Uuid
 import Uuid from 'uuid/v4';
 // Redux
@@ -8,15 +8,19 @@ import { addPersonAction } from '../actions/formActions';
 // PropTypes
 import PropTypes from 'prop-types';
 
-export class Form extends Component {
+export const Form = (props) => {
+
+    // Ref Hooks
+    const inputFirstName = useRef(null);
+    const inputLastName = useRef(null);
 
     // Submit the form
-    handleSubmit = (e) => {
+    function handleSubmit(e) {
         e.preventDefault();
         // Let's get value of all fields
         let id = Uuid();
-        let firstName = this.refs.inputFirsName.value;
-        let lastName = this.refs.inputLastName.value;
+        let firstName = inputFirstName.current.value;
+        let lastName = inputLastName.current.value;
 
         // Store the data
         const data = {
@@ -34,41 +38,39 @@ export class Form extends Component {
             console.log("lastName's field is empty");
         } else {
             // Let's call the actionCreator
-            this.props.addPersonAction(data);
+            props.addPersonAction(data);
 
             // Empties the fields
-            this.refs.inputFirsName.value = '';
-            this.refs.inputLastName.value = '';
+            inputFirstName.current.value = '';
+            inputLastName.current.value = '';
         }
     };
 
-    render() {
 
-        return (
-            <div className="form-container">
-                <h2 className="form-container__title">Person Form</h2>
-                <form className="form-container__form" onSubmit={this.handleSubmit}>
-                    <p>Firstname :</p>
-                    <input 
-                        className="form-container__inputFirstName"
-                        type="text"
-                        ref="inputFirsName"
-                        placeholder="firstname" 
-                        value={this.props.firstName}
-                    />
-                    <p>Lastname :</p>
-                    <input 
-                        className="form-container__inputLastName"
-                        type="text"
-                        ref="inputLastName"
-                        placeholder="lastname"
-                        value={this.props.lastName}
-                    />
-                    <button className="form-container__submit">Submit</button>
-                </form>
-            </div>
-        )
-    };
+    return (
+        <div className="form-container">
+            <h2 className="form-container__title">Person Form</h2>
+            <form className="form-container__form" onSubmit={handleSubmit}>
+                <p>Firstname :</p>
+                <input
+                    className="form-container__inputFirstName"
+                    type="text"
+                    ref={inputFirstName}
+                    placeholder="firstname"
+                    value={props.firstName}
+                />
+                <p>Lastname :</p>
+                <input
+                    className="form-container__inputLastName"
+                    type="text"
+                    ref={inputLastName}
+                    placeholder="lastname"
+                    value={props.lastName}
+                />
+                <button className="form-container__submit">Submit</button>
+            </form>
+        </div>
+    )
 }
 
 
