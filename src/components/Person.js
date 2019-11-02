b5d543fbf36a08037007baad4d9fa20536f7bdfc
Christopher <christopher.loubassou@hotmail.fr>
2019-11-02 18:20:33 +0100

    Change person component to fonctionnal component

--- src/components/Person.js
+++ src/components/Person.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 // Redux
 import { connect } from 'react-redux';
 // Action Types
@@ -6,48 +6,46 @@ import { removePersonAction, editPersonAction } from '../actions/formActions';
 // PropTypes
 import PropTypes from 'prop-types';
 
-export class Person extends Component {
+export const Person = (props) => {
 
-    handleRemovePerson = () => {
+    function handleRemovePerson() {
         // Send the id of person to param
-        let idPerson = this.props.person.id;
-        this.props.removePerson(idPerson)
+        let idPerson = props.person.id;
+        props.removePerson(idPerson)
     };
 
-    handleEditPerson = () => {
+    function handleEditPerson() {
         // Send the id of person to param
-        let idPerson = this.props.person.id;
-        this.props.editPerson(idPerson)
+        let idPerson = props.person.id;
+        props.editPerson(idPerson)
     };
 
-    render() {
 
-        // Let's destructuring
-        const { firstName, lastName } = this.props.person;
+    // Let's destructuring
+    const { firstName, lastName } = props.person;
 
-        return (
-            <div>
-                {
-                    // View of the person
-                    <div className="person-container">
-                        <li className="person-container__firstName">
-                            <p className="person-container__label">FirstName:</p> 
-                            
-                            <p className="person-container__person">{firstName}</p>    
-                        </li>
-                        <li className="person-container__lastName">
-                            <p className="person-container__label">LastName:</p>
-                            <p className="person-container__person">{lastName}</p>   
-                        </li>
-                        <div className="person-container__buttons">
-                            <button className="person-container_edit" onClick={this.handleEditPerson}>Edit</button>
-                            <button className="person-container_remove" onClick={this.handleRemovePerson}>Remove</button>
-                        </div>
+    return (
+        <div>
+            {
+                // View of the person
+                <div className="person-container">
+                    <li className="person-container__firstName">
+                        <p className="person-container__label">FirstName:</p>
+
+                        <p className="person-container__person">{firstName}</p>
+                    </li>
+                    <li className="person-container__lastName">
+                        <p className="person-container__label">LastName:</p>
+                        <p className="person-container__person">{lastName}</p>
+                    </li>
+                    <div className="person-container__buttons">
+                        <button className="person-container_edit" onClick={handleEditPerson}>Edit</button>
+                        <button className="person-container_remove" onClick={handleRemovePerson}>Remove</button>
                     </div>
-                }
-            </div>
-        );
-    }
+                </div>
+            }
+        </div>
+    );
 }
 
 // Let's change function to props
