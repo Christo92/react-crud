0c0a6ad53d42c9ab3c4cc251945c7340f38baf57
Christopher <christopher.loubassou@hotmail.fr>
2019-11-02 18:35:27 +0100

    EditForm component change to fonctionnal component

--- src/components/EditForm.js
+++ src/components/EditForm.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useRef } from 'react';
 // Redux
 import { connect } from 'react-redux';
 // Action Types
@@ -6,55 +6,55 @@ import { updatePersonAction } from '../actions/formActions';
 // PropTypes
 import PropTypes from 'prop-types';
 
-export class EditForm extends Component {
+export const EditForm = (props) => {
+
+    const inputFirstName = useRef(null);
+    const inputLastName = useRef(null);
 
     // To update the person we selected
-    handleUpdatePerson = (e) => {
+    function handleUpdatePerson(e) {
         e.preventDefault();
         // First we get the new value with refs
-        let updateFirstName = this.refs.updateFirstName.value;
-        let updateLastName = this.refs.updateLastName.value;
-        let idPerson = this.props.person.id;
+        let updateFirstName = inputFirstName.current.value;
+        let updateLastName = inputLastName.current.value;
+        let idPerson = props.person.id;
 
         // Let's store the new data
         const data = {
             updateFirstName,
             updateLastName
         };
-        
+
         // Then we call the action creator
-        this.props.updatePerson(idPerson, data);
+        props.updatePerson(idPerson, data);
     };
 
-    render() {
+    // Destructuring
+    let { firstName, lastName } = props.person;
 
-        // Destructuring
-        let { firstName, lastName } = this.props.person;
-
-        return (
-            <div className="editform-container">
-                <p className="editform-container__title">Edit this person</p>
-                <form className="editform-container__form" onSubmit={this.handleUpdatePerson}>
-                    <p className="editform-container__updateFirstName">FirstName:</p>
-                    <input
-                        className="editform-container__inputFirstName"
-                        type="text"
-                        ref="updateFirstName"
-                        placeholder={firstName}
-                    />
-                    <p className="editform-container__updateLastName">LastName:</p>
-                    <input
-                        className="editform-container__inputLastName"
-                        type="text"
-                        ref="updateLastName"
-                        placeholder={lastName}
-                    />
-                    <br /><br />
-                    <button className="editform-container__updateButton">Update</button>
-                </form>
-            </div>
-        );
-    }
+    return (
+        <div className="editform-container">
+            <p className="editform-container__title">Edit this person</p>
+            <form className="editform-container__form" onSubmit={handleUpdatePerson}>
+                <p className="editform-container__updateFirstName">FirstName:</p>
+                <input
+                    className="editform-container__inputFirstName"
+                    type="text"
+                    ref={inputFirstName}
+                    placeholder={firstName}
+                />
+                <p className="editform-container__updateLastName">LastName:</p>
+                <input
+                    className="editform-container__inputLastName"
+                    type="text"
+                    ref={inputLastName}
+                    placeholder={lastName}
+                />
+                <br /><br />
+                <button className="editform-container__updateButton">Update</button>
+            </form>
+        </div>
+    );
 }
 
 const mapDispatchToProps = dispatch => {
